0e0f8120591928b563a39a6e663abe7410a755d8
maruki00 <aitbaha48@gmail.com>
2025-06-26 22:04:31 +0100

    memory: object pooling

---
+++ memory/pooling/pool_benchmark_test.go
@@ -0,0 +1,39 @@
+package memory
+
+import (
+	"bytes"
+	"testing"
+)
+
+// type Data struct {
+// 	Value int
+// }
+//
+// var dataPool = sync.Pool{
+// 	New: func() any {
+// 		return &Data{}
+// 	},
+// }
+//
+// var bufferPool = sync.Pool{
+// 	New: func() any {
+// 		return new(bytes.Buffer)
+// 	},
+// }
+
+func BenchmarkIO(b *testing.B) {
+	for b.Loop() {
+		buf := bufferPool.Get().(*bytes.Buffer)
+		buf.Reset()
+		buf.WriteString("Hello, pooled world!")
+		bufferPool.Put(buf)
+	}
+}
+
+func BenchmarkObj(b *testing.B) {
+	for b.Loop() {
+		obj := dataPool.Get().(*Data)
+		obj.Value = 42
+		dataPool.Put(obj)
+	}
+}
