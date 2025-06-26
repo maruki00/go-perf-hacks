e3391719b0b570a84b94fcee44c9d89a76f978c6
maruki00 <aitbaha48@gmail.com>
2025-06-26 20:08:08 +0100

    memory: object pooling

--- memory/obj_pooling.go
+++ memory/obj_pooling.go
@@ -1,4 +1,41 @@
 package memory
 
+import (
+	"bytes"
+	"fmt"
+	"sync"
+	"testing"
+)
 
+type Data struct {
+	Value int
+}
 
+var dataPool = sync.Pool{
+	New: func() any {
+		return &Data{}
+	},
+}
+
+var bufferPool = sync.Pool{
+	New: func() any {
+		return new(bytes.Buffer)
+	},
+}
+
+func io_test(t *testing.T) {
+	buf := bufferPool.Get().(*bytes.Buffer)
+	buf.Reset()
+	buf.WriteString("Hello, pooled world!")
+	fmt.Println(buf.String())
+	bufferPool.Put(buf) 
+}
+
+func obj_test(t *testing.T) {
+	for range 1000000 {
+		obj := dataPool.Get().(*Data) 		
+		obj.Value = 42                
+		dataPool.Put(obj)             
+	}
+	fmt.Println("Done")
+}
